b59d6a27faef846adda4e56f8cc9679c7ae21649
Artem Yagodnik <artemyagodnik@gmail.com>
2025-07-02 19:27:04 +0300

    Windows implementation

---
+++ src/platform/PlatformServiceWin.cpp
@@ -0,0 +1,30 @@
+#include "PlatformServiceWin.h"
+
+void PlatformServiceWin::ShowWindow(const QRect &icon) {
+  if (window_ == nullptr) {
+    qDebug() << "Nullptr window";
+    return;
+  }
+
+  // TODO: Add taskbar position here
+  window_->setPosition(
+    icon.x(),
+    icon.y() - window_->height() - 50
+  );
+
+  window_->show();
+  window_->raise();
+  window_->requestActivate();
+}
+
+void PlatformServiceWin::ShowAsApp() {
+  if (window_ != nullptr) {
+    window_->setFlags(Qt::Window);
+  }
+}
+
+void PlatformServiceWin::ShowOnlyInTray() {
+  if (window_ != nullptr) {
+    window_->setFlags(Qt::FramelessWindowHint);
+  }
+}
